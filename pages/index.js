381093ec2e2bdb6a586e243a0ae0cce33202e068
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-11-02 14:03:55 -0500

    Minor changes to the grammar on the readme index page.

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,13 @@ export default function Home() {
           </h2>
 
           <p>
-            The web app you are using is a demonstration of a production tool I
-            built for a health system&apos;s supply chain team in 2021. I built
-            this demo app to serve as a point of reference and to help visualize
-            and explain how this process works.
+            The web app you are using is a demonstration of a tool that was
+            built for a health system&apos;s supply chain team in 2021. This
+            demo app was built to serve as a point of reference and to help
+            visualize and explain how this process works.
             <span className="font-semibold pl-1">
               The data that is used within this app has been randomly generated
-              and contains no sensitive or propierity information.
+              and contains no sensitive or proprietary information.
             </span>
           </p>
 
@@ -29,11 +29,12 @@ export default function Home() {
             The scenario that this demo app portrays is the following. You are a
             supply chain director for a local hospital. Every week on Sunday
             night, the backend of the production tool generates a list of five
-            items that it has deemed to be held in excess of what your
-            historical demand has shown. You are emailed that there are five new
-            items for you to review and reset. You then follow the link on the
-            email to this site where you can start resetting your inventory
-            levels on the &quot;Reset&quot; tab.
+            items that the system's algorithm has deemed to be held in excess of
+            what your historical demand has shown. You find an email in your
+            inbox telling you that there are five new items for you to review
+            and reset. You then follow the link on the email to this site where
+            you can start resetting your inventory levels on the
+            &quot;Reset&quot; tab.
           </p>
 
           <h4 className="text-xl font-semibold">Specs</h4>
@@ -43,10 +44,10 @@ export default function Home() {
             that is written in Python using the Django Rest Framework. Data is
             saved using a PostgreSQL cluster which is managed by AWS. The
             frontend client is very similiar to the demo app you are using right
-            now. It is a SPA using the React library with state management using
-            Redux. To help simplify the demo app, it is disconnected from a
-            backend and is maintaining all data within the client using Redux.
-            Simply refresh the browser to start over.
+            now. It is a Single Page Application using the React library with
+            state management using Redux. To help simplify the demo app, it is
+            disconnected from a backend and is maintaining all data within the
+            client using Redux. Simply refresh the browser to start over.
           </p>
 
           <p>
